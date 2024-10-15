33ae546a6e3fdf003199e5be3ef352446a0da896
GRAVEL-AI <Isaiah.gravette@gmail.com>
2024-10-14 18:09:29 -0700

    More changes

--- goblin-and-rat/ratscript.gd
+++ goblin-and-rat/ratscript.gd
@@ -1,11 +1,23 @@
-extends Node2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
