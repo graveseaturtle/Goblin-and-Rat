9fafc8f0ad3f0334aefbc503fa37ec3444c764d0
GRAVEL-AI <Isaiah.gravette@gmail.com>
2024-10-14 18:36:34 -0700

    bodies, physics, movement, a world border, beginning of a script

--- goblin-and-rat/ratscript.gd
+++ goblin-and-rat/ratscript.gd
@@ -1,3 +1,5 @@
+extends CharacterBody2D
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -9,12 +11,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("RatJump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("RatMoveLeft", "RatMoveRight")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
